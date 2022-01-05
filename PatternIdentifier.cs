66632f41036240e0ce8bd4256a2a9259b3c7c410
az58999 <az58999@AAK4000382>
2022-01-05 12:53:40 +0100

    Add project files.

---
+++ PatternIdentifier.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FilePatternIdentifier
+{
+    class PatternIdentifier
+    {
+        // Returns the first n_bytes of the file specified by path.
+        private byte[] getBytes(string path, int n_bytes)
+        {
+            byte[] buffer = new byte[n_bytes];
+            FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            int bytes_read = fileStream.Read(buffer, 0, n_bytes);
+            fileStream.Close();
+
+            return buffer;
+        }
+        // Prints the filename and first n_bytes of each file located in root_path.
+        public void IdentifyPatterns(string root_path, int n_bytes)
+        {
+            string [] filePaths = Directory.GetFiles(root_path, "", SearchOption.TopDirectoryOnly);
+            foreach (string file_path in filePaths)
+            {
+                byte[] buffer = getBytes(file_path, n_bytes);
+                string hex_string = Convert.ToHexString(buffer);
+                FileInfo fileInfo = new FileInfo(file_path);
+
+                Console.WriteLine(fileInfo.Name + ": " + hex_string);
+            }
+        }
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FilePatternIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PatternIdentifier Identifier = new PatternIdentifier();
+            Console.WriteLine("Please specify a directory: ");
+            string directory = Console.ReadLine();
+            
+            Console.WriteLine("Specify the number of bytes to read from each file");
+            string n_bytes_input = Console.ReadLine();
+            int n_bytes;
+
+            try
+            {
+                n_bytes = int.Parse(n_bytes_input);
+            }
+
+            catch(FormatException)
+            {
+                Console.WriteLine(String.Format("The entered value {0} is not a valid number", n_bytes_input));
+                Console.WriteLine("Reading 8 bytes from each file as default");
+                n_bytes = 8;
+            }
+
+           Identifier.IdentifyPatterns(directory, n_bytes);
+           Console.WriteLine("\nPress enter to exit the application"); 
+           Console.ReadLine();
+        }
+    }
+}
